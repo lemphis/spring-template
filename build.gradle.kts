cc1f17b5b3c0b732357f6961c9bf105029e65065
lemphis <lemphis@gmail.com>
2024-04-29 23:27:26 +0900

    Add Kotlin JDSL, update spring boot version
    * Kotlin JDSL 라이브러리 추가
    * 스프링 부트 버전 변경 (3.2.3 -> 3.2.5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	val kotlinVersion = "1.9.20"
 
-	id("org.springframework.boot") version "3.2.3"
+	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
 	kotlin("jvm") version kotlinVersion
@@ -45,6 +45,8 @@ val destDocsFilePath = "build/resources/main/static/docs"
 val copyDocumentTaskName = "copyDocument"
 val jarName = "spring-template.jar"
 val mysqlVersion = "8.0.29"
+val jdslVersion = "3.3.1"
+val jdslStarterVersion = "2.2.1.RELEASE"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -52,6 +54,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.linecorp.kotlin-jdsl:jpql-dsl:$jdslVersion")
+	implementation("com.linecorp.kotlin-jdsl:jpql-render:$jdslVersion")
+	implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:$jdslVersion")
+	implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter-jakarta:$jdslStarterVersion")
 	runtimeOnly("mysql:mysql-connector-java:$mysqlVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
